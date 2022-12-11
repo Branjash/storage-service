deedf799609c918c570a731ede461e921c8c8ed5
eefbf81 <branko.rovcanin@mckesson.com>
2022-12-11 18:16:24 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:11
+LABEL maintainer="branko_rovcanin@epam.com"
+VOLUME /storage-service
+COPY ./target/storage-service-0.0.1-SNAPSHOT.jar /usr/app/
+EXPOSE 8880
+WORKDIR /usr/app
+ENTRYPOINT ["java","-jar", "storage-service-0.0.1-SNAPSHOT.jar"]
---
+++ src/test/java/com/epam/epmcacm/storageservice/StorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.epmcacm.storageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
